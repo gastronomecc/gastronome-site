51d5fec6256e8f0ab5d0e50d4462cbfb072b1d3b
Ning <wanninglim@gmail.com>
2019-01-30 22:07:47 +0800

    Add individual recipes feature

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,30 +1,48 @@
 $(document).ready(function() {
-    retrieveData();
+    retrieveData()
 });
 
 // All recipes and their information
 var recipes = [];
 
 function retrieveData() {
+    var pageIndex = -1;
+
     var ref = firebase.database().ref("Recipes");
 
     ref.on('value', function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
             var value = childSnapshot.val();
-            var recipeName = value.title;
-            var recipeDesc = value.shortDesc;
-            var recipeImage = value.imageURL;
 
-            var object = {name: recipeName, description: recipeDesc, image: recipeImage}
+            var nutrition = [value.calories, value.carbs, value.protein, value.fats, value.salt, value.sugar];
+            pageIndex += 1;
+
+            var object = {
+                name: value.title, 
+                description: value.shortDesc, 
+                image: value.imageURL,
+                ingredients: value.ingredients,
+                instructions: value.instructions,
+                nutrition: nutrition,
+                pageIndex: pageIndex
+            }
             recipes.push(object);
         });
-        displayAllRecipes(recipes)
+
+        var url = location.pathname;
+        var filename = url.substring(url.lastIndexOf('/')+1);
+    
+        if (filename === "recipes.html") {
+            displayAllRecipes(recipes);
+        }
+        else {
+            displayRecipeInformation(recipes);
+        }
     });
 }
 
 // Display all recipes
 function displayAllRecipes(recipes) {
-    console.log(recipes.length);
     for (var i = 0; i < recipes.length; i++) {
         var contentDiv = document.createElement("div");
         contentDiv.setAttribute("class", "content--recipe");
@@ -48,7 +66,7 @@ function displayAllRecipes(recipes) {
         img1.setAttribute("class", "recipe--img");
 
         var recipeTextDiv = document.createElement("div");
-        recipeTextDiv.setAttribute("class", "recipe--text--div");
+        recipeTextDiv.setAttribute("class", "recipe--text--div")
 
         // Adding the description text
         var node2 = document.createElement("p");
@@ -71,10 +89,78 @@ function displayAllRecipes(recipes) {
         recipeTextDiv.appendChild(img2);
         recipeTextDiv.appendChild(contentDescription);
 
-
         contentDiv.appendChild(recipeTextDiv);
 
         // ------------ APPEND WHOLE DIV CONTENT ------------
         $("#content--container").append(contentDiv);
     }
+
+    findRecipeIndexClicked(); 
+}
+
+var index;
+
+// Find index of clicked recipe
+function findRecipeIndexClicked() {
+    $(".content--recipe").click(function() {
+        index = $(this).index();
+        console.log(index);
+
+        window.location.href = "individual-recipe.html?index=" + index;
+    });
+}
+
+
+// Render stuff on individual-recipe.html
+function displayRecipeInformation(recipes) {
+    // GET PARAMETER FOR CURRENT INDEX
+    url_string = window.location.href;
+    var url = new URL(url_string);
+    var currentIndex = url.searchParams.get("index");
+    console.log("first Index: " + currentIndex);
+
+
+    for (var i = 0; i < recipes.length; i++) {
+        if (currentIndex == i) {
+            var recipeTitle = recipes[i].name;
+            var recipeImage = recipes[i].image;
+            var recipeDescription = recipes[i].description;
+            var recipeIngredients = recipes[i].ingredients;
+            var recipeInstructions = recipes[i].instructions;
+            var recipeNutrition = recipes[i].nutrition;
+            break;
+
+            // ------------------- HOME CONTAINER -------------------
+            // var homeDiv = document.createElement("div");
+            // homeDiv.setAttribute("class", "home--div");
+
+            // // Adding a recipe image
+            // var img1 = new Image();
+            // img1.src = recipes[i].image;
+            // img1.setAttribute("class", "home--img");
+
+            // // Adding the title text
+            // var node1 = document.createElement("h1");
+            // var textnode1 = document.createTextNode(recipeTitle);
+            // node1.appendChild(textnode1);
+            // node1.setAttribute("class", "home--title");
+
+            // homeDiv.appendChild(img1);
+            // homeDiv.appendChild(node1);
+
+            // $(".home--container").append(homeDiv);
+
+            // ------------------- INSTRUCTIONS CONTAINER -------------------
+
+        }
+    }
+
+    $(".home--title").html(recipeTitle);
+    $(".home--image").attr("src", recipeImage);
+    console.log(recipeImage);
+
+    $(".home--image").prop("alt", recipeTitle);
+    $(".recipe--ingredients").html(recipeIngredients);
+    $(".recipe--instructions").html(recipeInstructions);
+    $(".recipe--nutrition").html(recipeNutrition);
 }
